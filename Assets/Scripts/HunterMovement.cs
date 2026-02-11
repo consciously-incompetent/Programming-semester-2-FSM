1a2c55cb5da727d22f727fc0bbf938db5a9d796b
Otis Murphy <murphyot@shernet.sheridancollege.ca>
2026-02-11 15:25:16 -0500

    added hunter movement

--- Assets/Scripts/HunterMovement.cs
+++ Assets/Scripts/HunterMovement.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class HunterMovement : MonoBehaviour
 {
+    public float speed;
+    public Vector3 test;
+    public GameObject ground;
+    float width;
+    float length;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        width = ground.GetComponent<MeshCollider>().bounds.size.x/2;
+        Debug.Log(width);
+         length = ground.GetComponent<MeshCollider>().bounds.size.z/2;
+        Debug.Log(length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        Debug.Log(pos.z);
+
+        if (Input.GetKey(KeyCode.D) && pos.x < width)
+        {
+            pos += Vector3.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A) && pos.x > -width)
+        {
+            pos += Vector3.left * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S) && pos.z < length)
+        {
+            pos += Vector3.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.W) && pos.z > -length)
+        {
+            pos += Vector3.back * Time.deltaTime * speed;
+        }
+
+
+
+        transform.position = pos;
+        //test = pos;
     }
 }
