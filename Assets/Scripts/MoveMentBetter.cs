90df809eefa696805ef80f77cd731a3d0380117d
Otis Murphy <murphyot@shernet.sheridancollege.ca>
2026-03-25 17:35:06 -0400

    made movement script

---
+++ Assets/Scripts/MoveMentBetter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MoveMentBetter : MonoBehaviour
+{
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xRate = Input.GetAxis("Horizontal");
+        float YRate = Input.GetAxis("Vertical");
+        
+
+
+
+
+
+
+
+
+    }
+}
